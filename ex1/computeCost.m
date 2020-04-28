449705a6fc96fc6a8f81f215c213444862d5fa89
prinzz1208 <priyanshu81212@gmail.com>
2020-04-28 11:28:05 +0530

    28/04/2020

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -9,6 +9,7 @@ s=0.0;
 % You need to return the following variables correctly 
 J = 0;
 
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
